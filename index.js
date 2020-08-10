e69a33f4a51dfe75981ffbb360cc37ef3839df82
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-08-10 00:59:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(num, name) {
     katzDeliLine.push(name)
-
+    num = katzDeliLine.length;
   return (`Welcome, ${name}. You are number ${num} in line.`)
 
 }
