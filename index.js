aad61820251a407c3e0e70f471c4a11292705fc7
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-08-10 01:07:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ function takeANumber(num, name) {
 
 function nowServing(katzDeliLine) {
   if(katzDeliLine.length > 0) {
-    katzDeliLine.slice(0, 1);
+    for(var i = 0; i < katzDeliLine.length; i++) {
     return "Currently serving " + katzDeliLine[0] + "." ;
-    
+    katzDeliLine.slice(0,1);
+  }
 
   }else {
     return `There is nobody waiting to be served!`;
