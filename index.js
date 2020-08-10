fdf21b4ca97ad2d9d1c158a65d5cb959a558d423
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-08-10 01:06:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ function takeANumber(num, name) {
 
 function nowServing(katzDeliLine) {
   if(katzDeliLine.length > 0) {
-    return "Currently serving " + katzDeliLine[0] + "." ;
     katzDeliLine.slice(0, 1);
+    return "Currently serving " + katzDeliLine[0] + "." ;
+    
 
   }else {
     return `There is nobody waiting to be served!`;
