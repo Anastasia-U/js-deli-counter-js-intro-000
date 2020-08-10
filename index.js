68ed097eec189932d02604be8e8fc8a3ee9e5bd8
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-08-10 00:53:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
 var katzDeliLine = [];
 
 function takeANumber(num, name) {
- num.push(name);
- return `Welcome, ${name}. You are number ${num.length} in line.`;
+  for(var i = 0; i < num.length; i++) {
+    return `Welcome, ${name[i]}. You are number ${num[i+1]} in line.`
+  }
+
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    return "Currently serving " + katzDeliLine.shift(1) + ".";
-
-  }
-  else {
-    return `There is nobody waiting to be served!`;
-
-  }
 
 }
 
 function currentLine(katzDeliLine) {
-  var p = "The line is currently: ";
 
-
-    if (katzDeliLine.length > 0) {
-      for (var i = 0; i < katzDeliLine.length; i++) {
-        p += (i+1) + ". " + katzDeliLine[i] + ", ";
-      }
-        return p.slice(0, p.length-2);
-
-    }
-    else {
-      return `The line is currently empty.`;
-    }
   }
